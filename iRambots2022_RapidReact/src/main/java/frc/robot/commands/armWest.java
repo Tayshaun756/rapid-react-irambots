cd42f040931068411d336ec1e78746a83bc9c820
Tayshaun756 <110211415+Tayshaun756@users.noreply.github.com>
2023-01-16 21:55:20 -0500

    Update armWest.java

--- iRambots2022_RapidReact/src/main/java/frc/robot/commands/armWest.java
+++ iRambots2022_RapidReact/src/main/java/frc/robot/commands/armWest.java
@@ -7,16 +7,16 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
 
-public class armWest extends Command {
+public class armWest extends CommandBase {
   
   public armWest() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
-    requires(Robot.armDirectional);
+    // Use addRequirements() here to declare subsystem dependencies
+    // eg. addRequirements(chassis);
+    addRequirements(Robot.armDirectional);
   }
 
   // Called just before this Command runs the first time
@@ -27,25 +27,25 @@ public class armWest extends Command {
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
+  public void execute() {
     Robot.armDirectional.armLeft();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() {
+  public boolean isFinished() {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() {
+  public void end(boolean interrupted) {
     Robot.armDirectional.stop();
   }
-  // Called when another command which requires one or more of the same
+  // Called when another command which addRequirements one or more of the same
   // subsystems is scheduled to run
-  @Override
+ /*  @Override
   protected void interrupted() {
     end();
-  }
+  } */
 }
