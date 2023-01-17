e85bd5476a52f0f64e75c4dc0fbfd0c56bb24151
Tayshaun756 <110211415+Tayshaun756@users.noreply.github.com>
2023-01-16 21:53:49 -0500

    Update armManual.java

--- iRambots2022_RapidReact/src/main/java/frc/robot/commands/armManual.java
+++ iRambots2022_RapidReact/src/main/java/frc/robot/commands/armManual.java
@@ -7,33 +7,33 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class armManual extends Command {
+public class armManual extends CommandBase {
     //@SuppressWarnings({ "PMD.UnusedPrivateField", "PMD.SingularField" })
 
     public armManual() {
-        requires(Robot.arm);
+        addRequirements(Robot.arm);
     }
 
     @Override
-    protected void execute (){
+    public void execute(){
         Robot.arm.armVert();
     }
     @Override
-    protected boolean isFinished() {
+    public boolean isFinished() {
         return false;
     }
 
     @Override
-    protected void end() {
+    public void end(boolean interrupted) {
         Robot.arm.stop();
     }
 
-    @Override
+    /* @Override
     protected void interrupted() {
         end();
     }
-
+*/
 }
