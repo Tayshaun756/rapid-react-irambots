5ff9816114e6e8b009cf3c6f80d07474b7dab8a5
Tayshaun756 <110211415+Tayshaun756@users.noreply.github.com>
2023-01-16 21:59:41 -0500

    Update hookWest.java

--- iRambots2022_RapidReact/src/main/java/frc/robot/commands/hookWest.java
+++ iRambots2022_RapidReact/src/main/java/frc/robot/commands/hookWest.java
@@ -3,14 +3,14 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Robot;
 
-public class hookWest extends Command {
+public class hookWest extends CommandBase {
   public hookWest() {
     // Use requires() here to declare subsystem dependencies
     // eg. requires(chassis);
-    requires(Robot.hookDirectional);
+    addRequirements(Robot.hookDirectional);
   }
 
   // Called just before this Command runs the first time
@@ -18,26 +18,26 @@ public class hookWest extends Command {
 
   // Called repeatedly when this Command is scheduled to run
   @Override
-  protected void execute() {
+  public void execute() {
     Robot.hookDirectional.hookLeft();
   }
 
   // Make this return true when this Command no longer needs to run execute()
   @Override
-  protected boolean isFinished() {
+  public boolean isFinished() {
     return false;
   }
 
   // Called once after isFinished returns true
   @Override
-  protected void end() {
+  public void end(boolean interrupted) {
     Robot.hookDirectional.stop();
 
   }
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
+ /*  @Override
+  public void interrupted() {
     end();
-  }
+  } */
 }
